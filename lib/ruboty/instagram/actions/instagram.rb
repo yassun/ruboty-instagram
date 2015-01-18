de63567a48fa03139231a09084a9dd171ad3adf0
Taga Yasunori <yasukun.taga@gmail.com>
2015-01-18 14:03:26 +0900

    add actions instagram

--- lib/ruboty/instagram/actions/instagram.rb
+++ lib/ruboty/instagram/actions/instagram.rb
@@ -1,9 +1,36 @@
+require "instagram"
+
 module Ruboty
   module Instagram
     module Actions
       class Instagram < Ruboty::Actions::Base
+        def initialize(message)
+          @message = message
+        end
+
         def call
-          message.reply("TODO: write a message.")
+          if url = search
+            message.reply(url)
+          end
+        end
+
+        def search
+          if media = response[0]
+            media.images.low_resolution.url
+          end
+        end
+
+        def response
+          instagram.tag_recent_media(URI.encode(message[:keyword]), :count => 1)
+        end
+
+        def instagram
+          ::Instagram.configure do |config|
+            config.client_id     = ENV["INSTAGRAM_CLIENT_ID"]
+            config.client_secret = ENV["INSTAGRAM_CLIENT_SECRET"]
+          end
+
+          ::Instagram.client
         end
       end
     end
