3fa4a841ab67b2340146c52c45dcef68593c738f
Taga Yasunori <yasukun.taga@gmail.com>
2015-01-18 13:00:23 +0900

    add on keyword

--- lib/ruboty/handlers/instagram.rb
+++ lib/ruboty/handlers/instagram.rb
@@ -3,7 +3,7 @@ require "ruboty/instagram/actions/instagram"
 module Ruboty
   module Handlers
     class Instagram < Base
-      on /instagram instagram/, name: 'instagram', description: 'TODO: write your description'
+      on /instagram (?<keyword>.+)/, name: 'instagram', description: 'Search image from instagram'
 
       def instagram(message)
         Ruboty::Instagram::Actions::Instagram.new(message).call
